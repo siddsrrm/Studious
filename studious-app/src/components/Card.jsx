f0869133a893c49075d051090f8a91908d35f659
Jai Barath <barathj@purdue.edu>
2026-02-20 13:50:15 -0500

    created Card component to make a mock homepage for when functionality added to login/register

---
+++ studious-app/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div className = "card">
+      <h2>Course</h2>
+      <p>study plan</p>
+    </div>
+  );  
+}
+
+export default Card
---
+++ studious-app/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import Card from './pages/Card.jsx'
+
+import './card.css';
+
+function App() {
+  
+  return (
+    <>
+      <Card/>
+      <Card/>
+    </>
+  );
+}
+
+export default App
