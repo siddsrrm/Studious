42c8fddb310c09638cc1560aa0337254f548a336
Jai Barath <barathj@purdue.edu>
2026-02-26 14:54:55 -0500

    added errors and input validation for login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,12 +1,67 @@
+import { Link } from 'react-router-dom'
 import styles from './Login.module.css'
+import { useState } from 'react'
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleEmailBlur = () => {
+    if (email === "") {
+      setEmailError("Email is required")
+    } else if (!email.includes("@") || !email.includes(".")) {
+      setEmailError("Email is invalid")
+    } else {
+      setEmailError("")
+    }
+  }
+  
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handlePasswordBlur = () => {
+    if (password === "") {
+      setPasswordError("Password is required")
+    } else if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters")
+    } else {
+      setPasswordError("")
+    }
+  }
+
+  const handleSubmit = (e) => {
+    handleEmailBlur()
+    handlePasswordBlur()
+
+    if (email === "" || password === "") {
+      return
+    } else if (!email.includes("@") || !email.includes(".")) {
+      return
+    } else if (password.length < 8 || confirmPassword.length < 8) {
+      return
+    }
+
+
+  }
+
   return (
     <div className={styles.container}>
       <h1>Studious</h1>
-      <input type="email" placeholder="Email" className={styles.input} />
-      <input type="password" placeholder="Password" className={styles.input} />
-      <button className={styles.button}>Login</button>
+      <input type="email" placeholder="Email" className={styles.input + (emailError ? " " + styles.inputError : "")} onChange={handleEmail} onBlur={handleEmailBlur}/>
+      {emailError && <p className={styles.error}>{emailError}</p>}
+
+      <input type="password" placeholder="Password" className={styles.input + (passwordError ? " " + styles.inputError : "")} onChange={handlePassword} onBlur={handlePasswordBlur}/>
+      {passwordError && <p className={styles.error}>{passwordError}</p>}
+
+      <button onClick={handleSubmit} className={styles.button}>Login</button>
       <p>Don't have an account? <Link to="/Register">Register</Link></p>
     </div>
   )
