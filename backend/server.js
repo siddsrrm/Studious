5fa64478b3469c5bccbb8f0db92d756ec94f91d6
Jai Barath <barathj@purdue.edu>
2026-02-26 18:11:52 -0500

    added my ip to the server so i could connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
+const { setServers } = require("dns/promises")
+setServers(["1.1.1.1", "8.8.8.8"]);
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/auth")
 
 const app = express();
 
@@ -9,6 +14,8 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res) => {
   res.send("API is running...");
