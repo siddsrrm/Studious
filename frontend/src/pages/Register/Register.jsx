bb56f834db49eff3d3f9c3890f078ae7f2722714
Jai Barath <barathj@purdue.edu>
2026-02-26 02:43:23 -0500

    added register UI and router link to login

---
+++ frontend/src/pages/Register/Register.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import styles from './Register.module.css'
+
+function Register() {
+  return (
+    <div className={styles.container}>
+      <h1>Studious</h1>
+      <input type="text" placeholder="First Name" className={styles.input} />
+      <input type="text" placeholder="Last Name" className={styles.input} />
+      <input type="email" placeholder="Email" className={styles.input} />
+      <input type="password" placeholder="Password" className={styles.input} />
+      <input type="password" placeholder="Confirm Password" className={styles.input} />
+      <button className={styles.button}>Register</button>
+      <p>Already have an account? <Link to="/login">Login</Link></p>
+    </div>
+  )
+}
+
+export default Register
