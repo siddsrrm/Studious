2b117917ec6a073b43f14f8b7bc6a4265573ea0f
Jai Barath <barathj@purdue.edu>
2026-02-26 18:12:17 -0500

    added mock homepage for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import Homepage from './pages/homepage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />}/>
-        <Route path="/Register" element={<Register />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/homepage" element={<Homepage />}/>
       </Routes>
     </BrowserRouter>
   )
