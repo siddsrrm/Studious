1457e2e3540413355a20a2d673b9e7294a3dc3a8
Kevin Luo <luo522@purdue.edu>
2026-04-03 12:16:17 -0400

    fixed bug where study plan would still exist in progress tracker after being deleted

--- backend/controllers/studyPlanController.js
+++ backend/controllers/studyPlanController.js
@@ -1,4 +1,6 @@
 const StudyPlan = require("../models/StudyPlan");
+const ProgressTracker = require("../models/ProgressTracker");
+const Task = require("../models/Task");
 
 // Create a new study plan 
 exports.createStudyPlan = async (req, res) => {
@@ -15,6 +17,8 @@ exports.createStudyPlan = async (req, res) => {
     });
 
     await newPlan.save();
+
+    // await ProgressTracker.recalculateAllUsers();
 
     res.status(201).json({ message: "Study plan created", studyPlan: newPlan });
   } catch (err) {
@@ -41,7 +45,22 @@ exports.deleteStudyPlan = async (req, res) => {
       return res.status(403).json({ message: "Forbidden" });
     }
 
+    await Task.deleteMany({ studyPlanID: plan._id });
+
     await StudyPlan.deleteOne({ _id: plan._id });
+
+    // Remove the deleted plan from the user's ProgressTracker and recalculate
+    const tracker = await ProgressTracker.findOne({ userID: req.user.userId });
+    if (tracker) {
+      tracker.planStats = tracker.planStats.filter(
+        ps => ps.studyPlan.toString() !== plan._id.toString()
+      );
+      tracker.totalTasks = tracker.planStats.reduce((sum, p) => sum + (p.totalTasks || 0), 0);
+      tracker.totalTasksFinished = tracker.planStats.reduce((sum, p) => sum + (p.completedTasks || 0), 0);
+      tracker.calculateUserScore();
+      await tracker.save();
+    }
+
     res.json({ message: "Study plan deleted" });
   } catch (err) {
     res.status(500).json({ error: err.message });
