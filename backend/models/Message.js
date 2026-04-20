cb80a3692bc3a93735b6ffa267b89e9566f60c1f
Jai Barath <barathj@purdue.edu>
2026-04-19 21:35:57 -0500

    added groupID reference to message schema

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose")
 
 const messageSchema = new mongoose.Schema({
+  // attribute, type
+  // ID can just be _id property of schema
+
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   receiverId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   content: { type: String, required: true },
   isRead: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  groupId: { type: mongoose.Schema.Types.ObjectId, ref: "StudyGroup", default: null }
 })
 
 module.exports = mongoose.model("Message", messageSchema)
